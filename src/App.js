09fd4b6346096231c49fd5be143a20931b0038e9
Shubh-Sad-Ratna <shubhamband2000@gmail.com>
2024-09-01 21:39:09 +0530

    Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React,{Fragment} from "react";
 import Header from "./Component/Layout/Header";
+import Meals from "./Component/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
