4ed2e85de7638646b56471febf83242afd70af24
Shubh-Sad-Ratna <shubhamband2000@gmail.com>
2024-09-02 12:29:39 +0530

    Making a hard Coded Cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React,{Fragment} from "react";
 import Header from "./Component/Layout/Header";
 import Meals from "./Component/Meals/Meals";
+import Cart from "./Component/Cart/Cart";
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
